072f383df30856c630e942f72d5a3b0b7674b5b6
innocent <innocentgodwin580@yahoo.com>
2025-07-16 11:52:19 +0930

    vpc updated

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -71,7 +71,7 @@ resource "aws_route_table_association" "public_subnet_az1_rt_association" {
 
 # associate public subnet az2 to public route table
 resource "aws_route_table_association" "public_subnet_az2_rt_association" {
-  route_table_id = aws_internet_gateway.internet_gateway.id 
+  route_table_id = aws_route_table.public_route_table.id  
   subnet_id = aws_subnet.public_subnet_az2.id  
 
 }
